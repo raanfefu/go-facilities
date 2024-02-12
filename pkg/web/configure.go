cca011efa0eac7e20311e072850c189d664f45d3
rfernandezo <rafael.fernandez.ve@gmail.com>
2024-02-12 03:20:59 -0500

    test library server

--- pkg/web/configure.go
+++ pkg/web/configure.go
@@ -9,7 +9,7 @@ import (
 func (s *impl) PreConfiguration() {
 	s.StringVar(&s.Params.ModeValue, "mode", "", "mode value is https / http")
 	s.UintVar(&s.Params.Port, "port", 0, "port using listing service")
-	//s.X509KeyPairVar(&s.Params.Certs, "tls", "certificado server")*/
+	s.X509KeyPairVar(&s.Params.Certs, "tls", "certificado server")
 }
 
 func (s *impl) PostConfiguration() error {
@@ -21,13 +21,11 @@ func (s *impl) PostConfiguration() error {
 		return errors.New("mode value must be http / https")
 	}
 	s.Params.Mode = vmode
-	/*
 
-
-		if *vmode == commons.TLS {
-			if s.Params.Certs.PrivateKey == nil {
-				return errors.New("no se puedo cargar el certificado 2")
-			}
-		}*/
+	if *vmode == common.TLS {
+		if s.Params.Certs.PrivateKey == nil {
+			return errors.New("no se puedo cargar el certificado 2")
+		}
+	}
 	return nil
 }
